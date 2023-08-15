0868378620ed8f81fc619d000b409a032cfc7913
Rômulo Viana <neoromulo@gmail.comgit config --global user.email neoromulo@gmail.comclgit config --global user.name Rmulo>
2023-08-15 15:24:25 -0300

    nova atualização

--- app/layouts/MainLayout.js
+++ app/layouts/MainLayout.js
@@ -1,23 +1,38 @@
 "use client";
 
-import React from "react";
-import Footer from "./includes/Footer";
+import TopMenu from "./includes/TopMenu";
 import MainHeader from "./includes/MainHeader";
 import SubMenu from "./includes/SubMenu";
-import TopMenu from "./includes/TopMenu";
+import Footer from "./includes/Footer";
+import Loading from "../components/Loading";
+import { useEffect, useState } from "react";
 
-function MainLayout({ children }) {
+export default function MainLayout({ children }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("storage", function () {
+      let res = localStorage.getItem("isLoading");
+      res === "false" ? setIsLoading(false) : setIsLoading(true);
+    });
+  });
+
   return (
     <>
-      <div id="Mainlayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
-        <TopMenu />
-        <MainHeader />
-        <SubMenu />
-        {children}
-        <Footer />
+      <div id="MainLayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
+        <div>
+          {isLoading ? <Loading /> : <div></div>}
+          <TopMenu />
+          <MainHeader />
+          <SubMenu />
+        </div>
+
+        <div>{children}</div>
+
+        <div>
+          <Footer />
+        </div>
       </div>
     </>
   );
 }
-
-export default MainLayout;
