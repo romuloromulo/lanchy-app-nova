c274468eed213de8177cf9d062a936f963f857f5
romuloromulo <neoromulo@gmail.com>
2024-04-27 20:25:18 -0300

    tornando as imagens de pizza locais depois de problemas com supabase bucket

--- prisma/seed.js
+++ prisma/seed.js
@@ -16,7 +16,7 @@ async function seedProducts() {
         categoria: "Salgada",
         description:
           "Clássica pizza italiana com molho de tomate, queijo mozzarella e folhas de espinafre fresco.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Espinafre.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9Fc3BpbmFmcmUucG5nIiwiaWF0IjoxNzEzOTE0MDkxLCJleHAiOjE3NDU0NTAwOTF9.h9f-Pyi0HrhQ5bljmtbA7wWujX95LAIPa9EoaWnEMDg&t=2024-04-23T23%3A14%3A50.422Z",
+        url: "/images/PizzasMenu/Espinafre.png",
       },
     });
 
@@ -30,7 +30,7 @@ async function seedProducts() {
 
         description:
           "Pizza coberta com generosas fatias de pepperoni sobre queijo derretido.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Pepperoni.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9QZXBwZXJvbmkucG5nIiwiaWF0IjoxNzEzOTE0MjQxLCJleHAiOjE3NDU0NTAyNDF9.i6gHyMDryuySW8WcA37p6OlI7Cx3uQDif5NPrZGBrHY&t=2024-04-23T23%3A17%3A20.214Z",
+        url: "/images/PizzasMenu/Pepperoni.png",
       },
     });
 
@@ -44,7 +44,7 @@ async function seedProducts() {
 
         description:
           "Pizza de calabresa com cebola e queijo mozzarella derretido.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Tomato.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9Ub21hdG8ucG5nIiwiaWF0IjoxNzEzOTE0MjU4LCJleHAiOjE3NDU0NTAyNTh9.wSZJAcC-TtcVpTGf7DdvOekEWpvclKOUmV_Yod5C8qU&t=2024-04-23T23%3A17%3A37.340Z",
+        url: "/images/PizzasMenu/Tomato.png",
       },
     });
 
@@ -64,7 +64,7 @@ async function seedProducts() {
         categoria: "Salgada",
 
         description: "Pizza da casa com presunto, picles, cebola e azeitonas.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Mixed.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9NaXhlZC5wbmciLCJpYXQiOjE3MTM5MTQyOTMsImV4cCI6MTc0NTQ1MDI5M30.twGc4TjmG_2iajxx8NWniUnNrLAmIQdfm5e0QDOF0OU&t=2024-04-23T23%3A18%3A12.550Z",
+        url: "/images/PizzasMenu/Mixed.png",
       },
     });
 
@@ -85,7 +85,7 @@ async function seedProducts() {
         categoria: "Salgada",
         description:
           "Pizza repleta de vegetais frescos, perfeita para os amantes de comida vegetariana.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Vegetariana.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9WZWdldGFyaWFuYS5wbmciLCJpYXQiOjE3MTM5MTQzMTQsImV4cCI6MTc0NTQ1MDMxNH0.idwL3aMyoB8aZo5wZiGmlYnZERzf4F-ZpD1pwSB_njw&t=2024-04-23T23%3A18%3A34.057Z",
+        url: "/images/PizzasMenu/Vegetariana.png",
       },
     });
 
@@ -99,7 +99,7 @@ async function seedProducts() {
 
         description:
           "Uma combinação do suave cogumelo e do salgado do presunto, molho e queijo derretido.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Presunto.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9QcmVzdW50by5wbmciLCJpYXQiOjE3MTM5MTQzMjcsImV4cCI6MTc0NTQ1MDMyN30.oJatddmdQ0UJlOq3j_s0QChtoDIGvR1M49dccuF2S8U&t=2024-04-23T23%3A18%3A47.121Z",
+        url: "/images/PizzasMenu/Presunto.png",
       },
     });
 
@@ -120,7 +120,7 @@ async function seedProducts() {
         categoria: "Salgada",
         description:
           "Inspirada na culinária mediterrânea, esta products apresenta ingredientes como azeitonas, tomate seco e queijo feta.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Azeitona.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9BemVpdG9uYS5wbmciLCJpYXQiOjE3MTM0ODE2NTAsImV4cCI6MTc0NTAxNzY1MH0.tYSZHoj_s3mOI3S41zQSD90vUtIkmpRpQ-QlK8mZFQE&t=2024-04-18T23%3A07%3A30.697Z",
+        url: "/images/PizzasMenu/Azeitona.png",
       },
     });
 
@@ -140,7 +140,7 @@ async function seedProducts() {
         categoria: "Salgada",
 
         description: "Pizza de cogumelo com azeitona,tomate e molho branco.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Presunto.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9QcmVzdW50by5wbmciLCJpYXQiOjE3MTM5MTQzMjcsImV4cCI6MTc0NTQ1MDMyN30.oJatddmdQ0UJlOq3j_s0QChtoDIGvR1M49dccuF2S8U&t=2024-04-23T23%3A18%3A47.121Z",
+        url: "/images/PizzasMenu/Cogumelo.png",
       },
     });
 
@@ -161,7 +161,7 @@ async function seedProducts() {
 
         description:
           "Pizza com pedaços suculentos de carne de churrasco, cebola e pimentão.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Churrasco.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9DaHVycmFzY28ucG5nIiwiaWF0IjoxNzEzOTE0Mzk0LCJleHAiOjE3NDU0NTAzOTR9.6CDXHMe8V98E33H7xzkxaq_YLa6XChyQAIH4PJHMk4w&t=2024-04-23T23%3A19%3A54.154Z",
+        url: "/images/PizzasMenu/Churrasco.png",
       },
     });
 
@@ -180,7 +180,7 @@ async function seedProducts() {
         categoria: "Doce",
         description:
           "Uma deliciosa products de sobremesa com chocolate derretido, morangos frescos e chocolate granulado.",
-        url: "https://qxejovzhjmcusbgwiklu.supabase.co/storage/v1/object/sign/PizzaImages/Chocolate.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQaXp6YUltYWdlcy9DaG9jb2xhdGUucG5nIiwiaWF0IjoxNzEzOTE0NDI3LCJleHAiOjE3NDU0NTA0Mjd9.VDvwrFJkvXFuXwGWoNNHCJrZHiNXtIDjxKt6_mBdrNQ&t=2024-04-23T23%3A20%3A26.525Z",
+        url: "/images/PizzasMenu/Chocolate.png",
       },
     });
   } catch (error) {
